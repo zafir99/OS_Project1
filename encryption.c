648ec7e7cfc78fdfa1ba211b9db612f489e0c54a
zaki <zafiruahmed@gmail.com>
2025-10-14 16:29:07 -0500

    rough draft of cipher

--- encryption.c
+++ encryption.c
@@ -1,2 +1,29 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+void changeCase(char *str, int len) {
+	for(int i = 0; i < len; ++i) {
+		str[i] = toupper(str[i]);
+	}
+}
+
+int main(int argc, char **argv) {
+	if (argc != 3) {
+		printf("Usage: encryption <original string> <encryption string>\n");
+		return(1);
+	}
+	/* First argument will be string to be encrypted, second argument will be encryption template */
+	int len1 = strlen(argv[1]);
+	int len2 = strlen(argv[2]);
+	/* preprocessing strings */
+	changeCase(argv[1], len1);
+	changeCase(argv[2], len2);
+	char encrypted[len1];
+	for (int i = 0; i < len1; ++i) {
+		char diff = (argv[2][i%len2] - 'A') + argv[1][i];
+		diff = 'A' + (diff - 'A')%26;
+		encrypted[i] = diff;
+	}
+	printf("%s\n", encrypted);
+}
