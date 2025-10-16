1379ab7a3a9012ae6c5fbd8384b81c140f623268
zaki <zafiruahmed@gmail.com>
2025-10-16 18:35:21 -0500

    finished command line cypher

--- encryption.c
+++ encryption.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 
@@ -9,21 +10,34 @@ void changeCase(char *str, int len) {
 }
 
 int main(int argc, char **argv) {
-	if (argc != 3) {
-		printf("Usage: encryption <original string> <encryption string>\n");
+	if (argc != 4) {
+		printf("Usage: encryption <original string> <encryption string><mode>\n");
+		printf("mode 0=encrypt, mode 1=decrypt\n");
 		return(1);
 	}
 	/* First argument will be string to be encrypted, second argument will be encryption template */
 	int len1 = strlen(argv[1]);
 	int len2 = strlen(argv[2]);
+	int mode = atoi(argv[3]);
 	/* preprocessing strings */
 	changeCase(argv[1], len1);
 	changeCase(argv[2], len2);
-	char encrypted[len1];
-	for (int i = 0; i < len1; ++i) {
-		char diff = (argv[2][i%len2] - 'A') + argv[1][i];
-		diff = 'A' + (diff - 'A')%26;
-		encrypted[i] = diff;
+	char encrypted[len1+1];
+	char diff;
+	if (mode) {
+		for (int i = 0; i < len1; ++i) {
+			diff = argv[1][i] - (argv[2][i%len2] - 'A');
+			diff = 'A' + (diff + 'A')%26;
+			encrypted[i] = diff;
+		}
 	}
+	else {
+		for (int i = 0; i < len1; ++i) {
+			diff = (argv[2][i%len2] - 'A') + argv[1][i];
+			diff = 'A' + (diff - 'A')%26;
+			encrypted[i] = diff;
+		}
+	}
+	encrypted[len1] = '\0';
 	printf("%s\n", encrypted);
 }
